071311b3e83432a5bb3259df79cb3691b62a4f33
Jakub Suchanek <suchanek989@seznam.cz>
2019-11-28 10:11:24 +0000

    add comments for flipping

--- IDP_arduino/drive.cpp
+++ IDP_arduino/drive.cpp
@@ -32,37 +32,37 @@ void drive_init() {
 #define SERVO_POSITION_LEVEL 0
 
 void pickup(bool top) {
-  MotorTop->run(BACKWARD);
+  MotorTop->run(BACKWARD); // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   MotorTop->setSpeed(100);
-  MotorBottom->run(BACKWARD);
+  MotorBottom->run(BACKWARD); // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   MotorBottom->setSpeed(100);
   if(!top) {
     servoPicker.write(SERVO_POSITION_BOTTOM);
   } else {
     servoPicker.write(SERVO_POSITION_TOP);
   }
-  delay(600);
+  delay(600); // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   MotorTop->setSpeed(0);
   MotorBottom->setSpeed(0);
   drive_velocity(3, 0);
-  delay(3000);
+  delay(3000); // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   servoPicker.write(SERVO_POSITION_LEVEL);
   drive_velocity(0, 0);
-  delay(300);
+  delay(300); // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 }
 
 void drop_off(bool top, bool flip) {
   drive_distance(10, 0);
   if(flip) {
     if(top) {
-      MotorTop->run(FORWARD);
+      MotorTop->run(FORWARD); // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
       MotorTop->setSpeed(100);
-      delay(1500);
+      delay(1500); // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
       MotorTop->setSpeed(0);
     } else {
-      MotorBottom->run(FORWARD);
+      MotorBottom->run(FORWARD); // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
       MotorBottom->setSpeed(100);
-      delay(1500);
+      delay(1500); // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
       MotorBottom->setSpeed(0);
     }
     top = !top;
@@ -72,17 +72,17 @@ void drop_off(bool top, bool flip) {
   } else {
     servoPicker.write(SERVO_POSITION_BOTTOM);
   }
-  MotorTop->run(FORWARD);
+  MotorTop->run(FORWARD); // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   MotorTop->setSpeed(100);
-  MotorBottom->run(FORWARD);
+  MotorBottom->run(FORWARD); // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   MotorBottom->setSpeed(100);
-  delay(1500);
+  delay(1500); // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   servoPicker.write(SERVO_POSITION_LEVEL);
-  MotorTop->run(BACKWARD);
+  MotorTop->run(BACKWARD); // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   MotorTop->setSpeed(100);
-  MotorBottom->run(BACKWARD);
+  MotorBottom->run(BACKWARD); // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   MotorBottom->setSpeed(100);
-  delay(1000);
+  delay(1000); // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   drive_distance(-10, 0);
 }
 
